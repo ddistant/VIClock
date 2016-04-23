89e4f2833bdc08ccff3be8f619f42d4245f6ee0d
Daniel Distant <daniel.distant1@gmail.com>
2016-04-22 22:14:06 -0400

    created timerContainer and stopwatchLabel, test environment complete, added to gitignore

--- VIClock/VIClock/ClockViewController.h
+++ VIClock/VIClock/ClockViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <PureLayout/PureLayout.h>
 
 @interface ClockViewController : UIViewController
 
--- VIClock/VIClock/ClockViewController.m
+++ VIClock/VIClock/ClockViewController.m
@@ -10,7 +10,11 @@
 
 @interface ClockViewController ()
 
-@property (nonatomic) UILabel *timeLabel;
+@property (nonatomic) ALView *timeContainer;
+@property (nonatomic) UILabel *stopwatchLabel;
+
+@property (nonatomic) BOOL didSetupTimeContainerConstraints;
+@property (nonatomic) BOOL didSetupStopwatchLabelConstraints;
 
 @end
 
@@ -18,12 +22,69 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    [self createTimeContainer];
+    [self createStopwatchLabel];
+    
 }
 
 -(void)viewDidAppear:(BOOL)animated {
-    self.view.layer.backgroundColor = [UIColor lightGrayColor].CGColor;
+    
+    [self setupTimeContainerConstraints];
+    [self setupStopwatchLabelConstraints];
 }
+
+#pragma mark - timeContainer
+
+-(void) createTimeContainer {
+    
+    self.timeContainer = [ALView newAutoLayoutView];
+    self.timeContainer.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.timeContainer];
+}
+
+-(void) setupTimeContainerConstraints {
+    
+    if (!self.didSetupTimeContainerConstraints) {
+        
+        [NSLayoutConstraint autoCreateAndInstallConstraints:^{
+            [self.timeContainer autoCenterInSuperview];
+            [self.timeContainer autoSetDimensionsToSize:CGSizeMake(200, 200)];
+        }];
+        
+        self.didSetupTimeContainerConstraints = YES;
+    }
+}
+
+#pragma mark - timeLabel
+
+-(void) createStopwatchLabel {
+    
+    self.stopwatchLabel = [[UILabel alloc] init];
+    self.stopwatchLabel.backgroundColor = [UIColor blackColor];
+    self.stopwatchLabel.text = @"00:00:00";
+    [self.stopwatchLabel setFont:[UIFont fontWithName:@"Arial" size:23.0]];
+    self.stopwatchLabel.textAlignment = NSTextAlignmentCenter;
+    self.stopwatchLabel.textColor = [UIColor whiteColor];
+    [self.view addSubview:self.stopwatchLabel];
+    
+}
+
+-(void) setupStopwatchLabelConstraints {
+    
+    if (!self.didSetupStopwatchLabelConstraints) {
+        
+        [NSLayoutConstraint autoCreateAndInstallConstraints:^{
+            [self.stopwatchLabel autoCenterInSuperview];
+            [self.stopwatchLabel autoSetDimensionsToSize:CGSizeMake(100, 75)];
+        }];
+        
+        self.didSetupStopwatchLabelConstraints = YES;
+    }
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
